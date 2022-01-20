e38d985365b0fbf4fa3f2ded4cbae77f34ced39f
kaioxdbrasil <kaioshm7@gmail.com>
2022-01-20 02:18:43 -0500

    modificações 20.01

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <a href="https://api.whatsapp.com/send/?phone=556892208766&text&app_absent=0" class="float" target="_blank">
 <i class="fa fa-whatsapp my-float"></i>
-</a>
+</a>-->
 <link rel="stylesheet" href="css/style.css" >
 
 <body>
@@ -16,17 +16,17 @@
     <div class="topleft">
         <p>
         <a href="https://www.almobrasil.com.br">
-        <img src="img/logo_top.png" alt="ALMO"  width="350" height="350">
+        <img src="img/logo_top.png" alt="ALMO"  width="250" height="250" >
         </a>
         </p>
     </div>
 </header>
 <main>
     <div class="middle">
-        <!--<a href="https://www.almobrasil.com.br">-->
-        <img src="img/logo_main.png" alt="ALMO" width= "1000" height="400">
+        <a href="https://www.almobrasil.com.br">
+        <img src="img/logo_main.png" alt="ALMO" class="responsive">
         </a>
-        <h1>EM BREVE UMA PERTO DE VOCÊ</h1>
+        <h1>EM BREVE, UMA PERTO DE VOCÊ</h1>
         <br>
         <a href="docs/almo_proposta_comercial.pdf">
         <button class="button button1">ENTENDA COMO FUNCIONA</button>
@@ -43,8 +43,8 @@
             <i class="fa fa-instagram"></i>
             </a>
             <a href="https://www.facebook.com/almobrasil" class="facebook"><i class="fa fa-facebook"></i></a>
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-            <!--<a href="#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+            <a href="#" class="whatsapp"><i class="fa fa-whatsapp"></i></a>
+            <!--<a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
             <a href="#" class="youtube"><i class="fa fa-youtube"></i></a> -->
         </div>
     </div>
